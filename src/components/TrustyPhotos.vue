8b312a6afbb93353e0a4a161e7541815bb61fc98
LouisLP <louis.lascelles.palys@gmail.com>
2024-11-01 23:25:38 +0100

    Config storage for photos

--- src/components/TrustyPhotos.vue
+++ src/components/TrustyPhotos.vue
@@ -1,32 +1,5 @@
 <script setup lang="ts">
-import japan1 from '@/assets/pics/japan_1.jpeg'
-import pacificSpirit from '@/assets/pics/pacific_spirit.jpg'
-import misfits from '@/assets/pics/misfits_black_bg.jpeg'
-import konigssee from '@/assets/pics/konigssee.jpg'
-import berlin1 from '@/assets/pics/berlin_1.jpg'
-
-const photos = [
-  {
-    src: japan1,
-    rotate: 'rotate-2',
-  },
-  {
-    src: pacificSpirit,
-    rotate: '-rotate-2',
-  },
-  {
-    src: misfits,
-    rotate: 'rotate-2',
-  },
-  {
-    src: konigssee,
-    rotate: 'rotate-2',
-  },
-  {
-    src: berlin1,
-    rotate: '-rotate-2',
-  },
-]
+import { photos } from '@/config/photos'
 </script>
 
 <template>
---
+++ src/config/photos.ts
@@ -0,0 +1,28 @@
+import japan1 from '@/assets/pics/japan_1.jpeg'
+import pacificSpirit from '@/assets/pics/pacific_spirit.jpg'
+import misfits from '@/assets/pics/misfits_black_bg.jpeg'
+import konigssee from '@/assets/pics/konigssee.jpg'
+import berlin1 from '@/assets/pics/berlin_1.jpg'
+
+export const photos = [
+  {
+    src: japan1,
+    rotate: 'rotate-2',
+  },
+  {
+    src: pacificSpirit,
+    rotate: '-rotate-2',
+  },
+  {
+    src: misfits,
+    rotate: 'rotate-2',
+  },
+  {
+    src: konigssee,
+    rotate: 'rotate-2',
+  },
+  {
+    src: berlin1,
+    rotate: '-rotate-2',
+  },
+]
