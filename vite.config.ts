6f6d88057110dd936f30ad09c9baf045757971ba
LouisLP <louis.lascelles.palys@gmail.com>
2024-11-03 15:06:15 +0100

    Attempting full path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue-portfolio/',
+  base: 'https://louislp.github.io/vue-portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
