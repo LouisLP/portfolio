2164711e1a27a2d1437fe30690943452ace5d79d
LouisLP <louis.lascelles.palys@gmail.com>
2024-11-04 21:07:23 +0100

    Attempting to use `createWebHashHistory` instead

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -19,7 +19,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/vue-portfolio/'), // Make this explicit to match vite.config.ts
+  history: createWebHashHistory('/vue-portfolio/'),
   routes,
   scrollBehavior(to, _from, savedPosition) {
     if (savedPosition) {
