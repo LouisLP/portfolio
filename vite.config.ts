f3df2994b4fff65bb75c9fda65bb966f1c01fa99
LouisLP <louis.lascelles.palys@gmail.com>
2024-11-07 19:56:13 +0100

    Making the build work

--- vite.config.ts
+++ vite.config.ts
@@ -12,16 +12,16 @@ export default defineConfig({
   },
   build: {
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1)
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img'
-          }
-          return `assets/${extType}/[name]-[hash][extname]`
-        },
-      },
-    },
+    // rollupOptions: {
+    //   output: {
+    //     assetFileNames: (assetInfo) => {
+    //       let extType = assetInfo.name.split('.').at(1)
+    //       if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+    //         extType = 'img'
+    //       }
+    //       return `assets/${extType}/[name]-[hash][extname]`
+    //     },
+    //   },
+    // },
   },
 })
